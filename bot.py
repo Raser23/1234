7da470bdf38d2399d8ce857ab181928e8ee99cb3
Yaroslav <kharchenko.yar@gmail.com>
2019-04-27 17:41:41 +0700

    1

--- bot.py
+++ bot.py
@@ -1,14 +1,13 @@
 class Bot:
     def __init__(self, implements):
         self.user_sessions = []
-        self.send_message = implements.send_message
+        self.send_message = implements["send_message"]
 
     def got_location(self, location):
         pass
 
     def got_text_message(self, msg):
         pass
-
 
     def got_command(self, cmd, user_id, text):
         if cmd == "start":
