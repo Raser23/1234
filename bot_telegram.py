343f6f0150e3780043d6d6958dc79303a6e26b8f
Raser23 <ser.neshadim@yandex.ru>
2019-04-27 13:52:12 +0300

    1234

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,14 +1,38 @@
 import config
+from bot import Bot
+
+
 print(config.BOT_TOKEN)
 import telebot
 
 bot = telebot.TeleBot(config.BOT_TOKEN)
 
-@bot.message_handler(commands=['start', 'help'])
+def get_user(message):
+	return message.chat.id
+
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	bot_wrap.got_command("start",get_user(message),"")
 
-user = bot.get_me()
-print(user)
+@bot.message_handler(content_types=["location"])
+def location_sent(message):
+    print(message.location)
+    location = message.location
+    bot_wrap.got_location(location)
 
+@bot.message_handler(content_types=["text"])
+def text_sent(message):
+    text = message.text
+    bot_wrap.got_text_message(text)
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+def send_message(chat_id, text):
+    bot.send_message(chat_id=chat_id, text=text)
+
+bot_wrap = Bot({'send_message' : send_message})
 bot.polling(none_stop=False, interval=0, timeout=20)
