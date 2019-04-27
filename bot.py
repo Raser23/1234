0901023dbfe12cee81bf39838801804836512734
Yaroslav <kharchenko.yar@gmail.com>
2019-04-27 17:36:36 +0700

    1

--- bot.py
+++ bot.py
@@ -1,11 +1,17 @@
 class Bot:
-    def __init__(self):
+    def __init__(self, implements):
         self.user_sessions = []
-
+        self.send_message = implements.send_message
 
     def got_location(self, location):
         pass
 
+    def got_text_message(self, msg):
 
-    def got_text_message(self, text):
-        pass
+
+
+    def got_command(self, cmd, user_id, text):
+        if cmd == "start":
+            self.user_sessions.append({'id': id})
+            self.send_message(user_id, "Hello!")
+
--- main.py
+++
@@ -1,7 +0,0 @@
-from bot import Bot
-from bot_telegram import tbot
-
-
-if __name__ == "__main__":
-    telegram_bot = Bot()
-
