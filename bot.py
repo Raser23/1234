06d2466a872376d89180affeaa94f7242a47058a
Yaroslav <kharchenko.yar@gmail.com>
2019-04-27 17:14:33 +0700

    1

---
+++ bot.py
@@ -0,0 +1,12 @@
+class Bot:
+    def __init__(self, bot_class):
+        self.user_sessions = []
+        self.bot_class = bot_class
+
+
+    def got_location(self, location):
+        pass
+
+
+    def got_text_message(self, text):
+        pass
---
+++ main.py
@@ -0,0 +1,7 @@
+from bot import Bot
+from bot_telegram import tbot
+
+
+if __name__ == "__main__":
+    telegram_bot = Bot()
+
